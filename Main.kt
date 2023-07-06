b1983f31d5f8f7b3d14b8c629666a1f002dc266b
Galina <djngalja@gmail.com>
2023-07-06 23:18:56 +0300

    Fixed an error. Made the code more readable.

--- Main.kt
+++ Main.kt
@@ -1,69 +1,32 @@
-/*PASSWORD STRENGTH TEST
+/* PASSWORD STRENGTH TEST
 
-Evaluates the strength of your password based on the following criteria:
-
-- length
-- lower- and uppercase letters
-- special characters
-- numbers
-- common patterns
-
-Input Format:
-A string containing a password.
-
-*/
+    SCORE:
+Length: +1 point for every 8 characters
++1 point for numbers
++1 point for capital letters
++1 point for lowercase letters
++1 point for every special character
+-1 point for every common pattern */
 
 fun main() {
     val pass = readln()
-    //val pass = "hdd_ht45Tvd"
-    //val pass = ":g8jzabcdefY9p}qwerty7d7vPX"
-    evaluate(pass)
-}
-
-fun evaluate(pass: String){
-    var score = 0
-
-//The longer, the better. A good password must be at least 8 characters long (score +1). Even better, if it's twice as long. +2 points, if the password is 16 characters long.
     println("password length = ${pass.length}\n")
-    score+= pass.length/8
 
-//+1 point, if the password contains numbers.
-    val numbers = checkChar(pass,48,57)//ACSII codes
-    printResult(numbers, "numbers")
-    score+=numbers
-
-//+1 point, if there are uppercase letters.
-    val capitals = checkChar(pass,65,90)
-    printResult(capitals, "capital letters")
-    score+=capitals
-
-//+1 point, if there are lowercase letters.
-    val lowercase = checkChar(pass,97,122)
-    printResult(lowercase, "lowercase letters")
-    score+= lowercase
-
-//An additional point for every special character.
+    var score = 0
+    val patterns = arrayOf("0000", "1111", "123123", "123321", "12345", "5555", "654321", "8888", "123qwe", "1q2w3e", "abc123", "abcdef", "admin", "dragon", "iloveyou", "lovely", "password", "qwerty", "welcome")
+    val numbers = checkChar(pass,48,57, "numbers") //ASCII codes
+    val capitals = checkChar(pass,65,90, "capital letters")
+    val lowercase = checkChar(pass,97,122, "lowercase letters")
     val characters = checkSpecialChar(pass)
-    if(characters==0) println("[ ] special characters")
-    else println("[x] $characters special character(s)")
-    score+= characters
 
-//Too lazy to come up with an original password? Too bad! -1 point for every common pattern found in your password.
-    val common = arrayOf("0000", "1111", "123123", "123321", "12345", "5555", "654321", "8888", "123qwe", "1q2w3e", "abc123", "abcdef", "admin", "dragon", "iloveyou", "lovely", "password", "qwerty", "welcome") //add popular password patterns to this array
-    for(i in common){
-        score+=findCommonPass(pass, i)
-    }
+    score+= pass.length/8 + numbers + capitals + lowercase + characters
+    for(pattern in patterns) score-= findCommonPat(pass, pattern)
 
-    val result = strength(score)
-    println("\ntotal score = $score ($result)")
+    println("\ntotal score = $score (${evalStrength(score)})")
 }
 
-fun printResult(num: Int, name: String){
-    if(num==0) println("[ ] $name")// Functions return 0, if nothing was found (no numbers / no special characters)
-    else println("[x] $name")
-}
 
-fun strength(score:Int):String{
+fun evalStrength(score:Int):String{
     return if(score<=2) "very weak"
     else if(score<=4) "weak"
     else if(score<=6) "medium"
@@ -71,42 +34,47 @@ fun strength(score:Int):String{
     else "very strong"
 }
 
-//This function checks whether a group of certain characters can be found in your password using their ASCII codes
-fun checkChar(pass: String, num1: Int, num2: Int): Int{
-
+//This function checks if a password contains numbers/lowercase/capital letters (using their ASCII codes)
+fun checkChar(pass: String, num1: Int, num2: Int, name: String): Int{
+    var found = 0
     for(i in pass){
-        if(i.code in num1..num2) return 1
+        if(i.code in num1..num2) found = 1
     }
-    return 0
+    if(found==0) println("[ ] $name")
+    else println("[x] $name")
+    return found
 }
 
 //This function looks for special characters and counts them
 fun checkSpecialChar(pass: String):Int{
     var count = 0
-    val special ="~@#$%^&*_-+=`|\\/(){}[]<>:;\"',.?!"
+    val specialChar ="~@#$%^&*_-+=`|\\/(){}[]<>:;\"',.?!"
     for(i in pass){
-        for(j in special){
+        for(j in specialChar){
             if(i==j) count++
         }
     }
+    if(count==0) println("[ ] special characters")
+    else println("[x] $count special character(s)")
     return count
 }
 
-//This function looks for common passwords such as "qwerty" and "12345"
-fun findCommonPass(pass: String, common: String): Int{
-    var size = 0
-    var count = 0
+//This function looks for common password patterns such as "qwerty", "12345", etc.
+fun findCommonPat(pass: String, pattern: String): Int{
+    var temp = ""
+    var found = 0
     for(i in 0 until pass.length){
-        if(pass[i]==common[0]){ //find the 1st letter of a pattern
-            for(j in 1 until common.length){
-                if(pass[i+j]==common[j]) size++
+        if(pass[i]==pattern[0] && (i+pattern.length-1)<=pass.length){ //find the 1st letter of a pattern
+            temp+= pattern[0]
+            for(j in 1 until pattern.length){
+                if(pass[i+j]==pattern[j]) temp+= pattern[j]
                 else break
             }
-            if(size==common.length-1){
-                println("[!] common pattern \"$common\" is found")
-                count++
+            if(temp==pattern){ //a common pattern is found
+                println("[!] common pattern \"$pattern\" is found")
+                found = 1
             }
         }
     }
-    return count*(-1)
+    return found
 }
